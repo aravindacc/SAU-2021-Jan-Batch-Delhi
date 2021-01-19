680e511eef06b02ce5e70318997a6109a67d3e92
Satyam-Accolite <77054237+Satyam-Accolite@users.noreply.github.com>
2021-01-20 01:35:35 +0530

    Update Sorting.java

--- JavaFundamentals-Morning/CollectionSorting/src/Sorting.java
+++ JavaFundamentals-Morning/CollectionSorting/src/Sorting.java
@@ -4,18 +4,25 @@ public class Sorting {
 	public static void main(String[] args) {
 		
 		Scanner scn = new Scanner(System.in);
-		List<String> list = new ArrayList<String>();  
+		List<Integer> list = new ArrayList<>();  
 		System.out.println("Enter number of inputs");
 		int n = scn.nextInt();
-		
-		for(int i=0;i<=n;i++) {
-			String str = scn.nextLine();
-			list.add(str);
+		System.out.println("Enter the list values");
+		for(int i=0;i<n;i++) {
+			int num = scn.nextInt();
+			list.add(num);
 		}
 		
-		Collections.sort(list);    //sorts array list  
-		for(String str: list)   
-		System.out.print(" "+str);  
-		    }
+		//Sorting list using priorityQueue which is a part of a collection
+		//sorts array list  
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		pq.addAll(list);
+		list= new ArrayList<>();
+		while(!pq.isEmpty()){
+		    list.add(pq.poll());
+		}
+		//printing the sorted list
+		System.out.println("The sorted list is:");
+        System.out.println(list);
+        }
 	}
-
