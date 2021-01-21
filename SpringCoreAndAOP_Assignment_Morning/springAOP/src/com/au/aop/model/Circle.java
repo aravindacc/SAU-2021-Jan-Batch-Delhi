88502904e4df04154f84cf0bafe97c6171cab447
Shubham191296 <77062854+Shubham191296@users.noreply.github.com>
2021-01-21 20:08:11 +0530

    Add files via upload

---
+++ SpringCoreAndAOP_Assignment_Morning/springAOP/src/com/au/aop/model/Circle.java
@@ -0,0 +1,19 @@
+package com.au.aop.model;
+
+public class Circle {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String setNameAndReturning(String name) {
+        this.name = name;
+        System.out.println("Setter Method and return the value method executed....");
+        return name;
+    }
+}
