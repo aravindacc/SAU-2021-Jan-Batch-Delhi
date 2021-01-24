6e0d015323002a8f151f6f7990d6bd47335dd167
Saachi <saachi.r@accolitedigital.com>
2021-01-24 22:03:47 +0530

    Added React Native Assignment

---
+++ Saachi/React Native Session-Morning&Evening/E-LOOK Application/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import UserDetail from "./components/UserDetail";
+import HomeScreen from "./components/HomeScreen";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="HomeScreen">
+                <Stack.Screen
+                  name="HomeScreen"
+                  component={HomeScreen}
+                  options={{ headerShown: false }}/>
+                <Stack.Screen 
+                  name="UserDetail" 
+                  component={UserDetail} 
+                  options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default App;
