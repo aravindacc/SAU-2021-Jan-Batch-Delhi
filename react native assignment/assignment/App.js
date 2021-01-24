e4a1af3b275f6319448a36c11cf3534072208118
jatin kumar <jatin.kumar@accolitedigital.com>
2021-01-24 12:04:28 +0530

    Added Splash Screen

--- react native assignment/assignment/App.js
+++ react native assignment/assignment/App.js
@@ -8,12 +8,44 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeComponent from './components/homeComponent';
 import ProductDetails from './components/ProductDetails'
 import { Header } from 'react-native/Libraries/NewAppScreen';
+// import * as SplashScreen from 'expo-splash-screen';
+
+
+function SplashScreen()
+{
+  return (
+    <View style={{ flexDirection:'column' ,height:'100%',widht:'100%',justifyContent:'center'}}>
+      <Text style={{flex:0.5,fontSize:20,color:'teal',textAlign:'center',textAlignVertical:'center'}}>React Native Assignment</Text>
+      <Text style={{flex:0.4,fontSize:15,color:'teal',textAlign:'center',textAlignVertical:'center'}}>By Jatin</Text>
+    </View>
+
+  );
+}
+
+
 const Stack=createStackNavigator();
 
 export default function App() {
 
+  const[isLoading,setIsLoading]=useState(true);
+
+
+  performTimeConsumingTask = () => {
+      setTimeout(
+        () => { setIsLoading(false) },
+        2000
+      );
+  }
+
+  performTimeConsumingTask();
+
+  if(isLoading)
+  {
+    return <SplashScreen/>;
+  }
+
+
   return (
-    
     <NavigationContainer>
       <Stack.Navigator >
         <Stack.Screen 
