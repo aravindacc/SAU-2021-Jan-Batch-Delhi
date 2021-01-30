cf3b260c655158850dd4c8ecabb6dd5087eac23d
Muskan Srivastava <muskan-srivastava>
2021-01-30 23:00:31 +0530

    Assignment Submission

---
+++ Kafka Introduction - Morning/KafkaAssignment/src/main/java/com/kafka/producer_consumer/engine/Producer.java
@@ -0,0 +1,30 @@
+package com.kafka.producer_consumer.engine;
+
+import com.kafka.producer_consumer.models.Movie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class Producer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private static final String TOPIC = "users";
+
+    @Autowired
+    private KafkaTemplate<String, Movie> kafkaTemplate;
+
+    public void sendMovie(String movieName, int year, String genre, String actor) {
+        Movie movie = new Movie();
+        movie.setMovieName(movieName);
+        movie.setYear(year);
+        movie.setGenre(genre);
+        movie.setActor(actor);
+        logger.info(String.format("#### -> Producing message -> %s", movie));
+        this.kafkaTemplate.send(TOPIC, movie);
+    }
+}
